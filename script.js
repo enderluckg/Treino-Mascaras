6f19300b0b5e97a9ac50002e0287a08eceff3bf1
Salman <153955485+enderluckg@users.noreply.github.com>
2025-09-25 07:41:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,185 @@
+
+
+
+
+
+ function mascara_cpf()
+ {
+
+    let cpf = document.getElementById("inpCPF").value
+
+
+    cpf = cpf.slice(0,14)
+
+    if(cpf[3]!=".")
+    {
+       if(cpf[3] != undefined){
+
+        document.getElementById("inpCPF").value = cpf.slice(0,3) + "." + cpf[3]
+       }
+    }
+
+    if(cpf[7]!=".")
+        {
+           if(cpf[7] != undefined){
+    
+            document.getElementById("inpCPF").value = cpf.slice(0,7) + "." + cpf[7]
+           }
+        }
+
+        if(cpf[11]!="-")
+            {
+               if(cpf[11] != undefined){
+        
+                document.getElementById("inpCPF").value = cpf.slice(0,11) + "-" + cpf[11]
+               }
+            }
+    
+    
+    console.log(cpf)
+
+
+ }
+
+ function mascara_num()
+ {
+
+    let num = document.getElementById("inpNUM").value
+
+    if(num[0]!="(")
+    {
+       if(num[0] != undefined){
+
+        document.getElementById("inpNUM").value = num.slice(0,0) + "(" + num[0]
+       }
+    }
+
+    if(num[3]!=")")
+        {
+           if(num[3] != undefined){
+    
+            document.getElementById("inpNUM").value = num.slice(0,3) + ")" + num[3]
+      
+           }
+        }
+
+        
+    if(num[5]!=" ")
+      {
+         if(num[5] != undefined){
+  
+          document.getElementById("inpNUM").value = num.slice(0,5) + " " + num[5]
+         }
+      }
+
+       
+
+        if(num[10]!=" ")
+            {
+               if(num[10] != undefined){
+        
+                document.getElementById("inpNUM").value = num.slice(0,10) + " " + num[10]
+               }
+            }
+
+
+        
+    
+    
+    console.log(num)
+
+        }
+
+
+        function mascara_cep()
+        {
+       
+           let num = document.getElementById("inpCEP").value
+       
+           if(num[2]!=".")
+           {
+              if(num[2] != undefined){
+       
+               document.getElementById("inpCEP").value = num.slice(0,2) + "." + num[2]
+              }
+           }
+       
+           if(num[6]!=".")
+               {
+                  if(num[6] != undefined){
+           
+                   document.getElementById("inpCEP").value = num.slice(0,6) + "." + num[6]
+             
+                  }
+               }
+       
+               
+       
+              
+       
+               if(num[10]!=".")
+                   {
+                      if(num[10] != undefined){
+               
+                       document.getElementById("inpCEP").value = num.slice(0,10) + "." + num[10]
+                      }
+                   }
+       
+       
+               
+           
+           
+           console.log(num)
+       
+               }
+
+
+               function mascara_cnpj()
+               {
+              
+                  let num = document.getElementById("inpCNPJ").value
+              
+                  if(num[2]!=".")
+                  {
+                     if(num[2] != undefined){
+              
+                      document.getElementById("inpCNPJ").value = num.slice(0,2) + "." + num[2]
+                     }
+                  }
+              
+                  if(num[6]!=".")
+                      {
+                         if(num[6] != undefined){
+                  
+                          document.getElementById("inpCNPJ").value = num.slice(0,6) + "." + num[6]
+                    
+                         }
+                      }
+              
+                      
+              
+                     
+              
+                      if(num[10]!="/")
+                          {
+                             if(num[10] != undefined){
+                      
+                              document.getElementById("inpCNPJ").value = num.slice(0,10) + "/" + num[10]
+                             }
+                          }
+
+                          if(num[14]!="-")
+                           {
+                              if(num[14] != undefined){
+                       
+                               document.getElementById("inpCNPJ").value = num.slice(0,14) + "-" + num[14]
+                              }
+                           }
+              
+              
+                      
+                  
+                  
+                  console.log(num)
+              
+                      }
